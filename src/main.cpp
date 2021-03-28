e0a7734c495d8bc54da676d0cdb2c53c2fdfb782
Rysiek Labus <rysieklabus@gmail.com>
2021-03-28 20:59:47 +0200

    working version

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 //
 // library
 #include <Arduino.h>
+#include <EEPROM.h>
 
 // inputs and outputs
 #define bo1               3                                     // water pump 1
@@ -11,10 +12,11 @@
 #define bi3               4                                     // push button 
 #define ai1               A0                                    // soil moisture
 
-#define CHILD_ID_BO1      1                                     // child ID for BO1 sensor                                     
-#define CHILD_ID_BI1      2                                     // child ID for BI1 sensor
-#define CHILD_ID_BI2      3                                     // child ID for BI2 sensor
-#define CHILD_ID_AI1      4                                     // child ID for AI1 sensor
+#define CHILD_ID_BO1      1                                     // child ID for BO1 sensor pump                            
+#define CHILD_ID_BI1      2                                     // child ID for BI1 sensor tank low
+#define CHILD_ID_BI2      3                                     // child ID for BI2 sensor tank full 
+#define CHILD_ID_AI1      4                                     // child ID for AI1 sensor moisture mesure percentage
+#define CHILD_ID_AO1      5                                     // child ID for AO1 sensor moisture setpoint percentage
 
 // mysensors config
 //#define MY_GATEWAY_SERIAL                                     // Enable serial gateway
@@ -22,11 +24,13 @@
 #define MY_DEBUG                                                // Enable debug prints to serial monitor
 #define MY_RADIO_NRF24                                          // Enable and select radio type attached
 #define MY_RF24_CHANNEL 80                                      // mysensors network channel
-#define MY_NODE_ID 21                                            // mysensors node id
+#define MY_NODE_ID 21                                           // mysensors node id
+//#define MY_TRANSPORT_WAIT_READY_MS 1000                       // pass if radio not ready
 
 boolean bo_state[2] = {false, false};
 boolean low_lvl = false;
 boolean high_lvl = false;
+boolean save_to_flash_flag = false;
 int moisture = 0;
 int moisture_set = 65;
 unsigned long bo1_start_time = 0;
@@ -34,6 +38,7 @@ unsigned long impulse_ml = 500;                                 // one impulse a
 unsigned long impulse_time = 38;                                // impulse time
 unsigned long last_moisture_send = 0;                           // moisture sending timer
 unsigned long last_moisture_read = 0;                           // pomiar co 6 sekund * 10 pomiarów w filtrze daje nam średnia z minuty
+unsigned long save_to_flash_after = 0;                          // save settings to flash after x time
 
 // mysensors library
 #include <MySensors.h>
@@ -42,6 +47,7 @@ MyMessage msgBO1(CHILD_ID_BO1, V_STATUS);
 MyMessage msgBI1(CHILD_ID_BI1, V_STATUS);
 MyMessage msgBI2(CHILD_ID_BI2, V_STATUS);
 MyMessage msgAI1(CHILD_ID_AI1, V_LEVEL);
+MyMessage msgAO1(CHILD_ID_AO1, V_PERCENTAGE);
 
 void BO_SET() {
   if(low_lvl == false){
@@ -68,9 +74,11 @@ void presentation(){
   sprintf(etykieta,"R%02u.BI1L",addr);  present(CHILD_ID_BI1, S_BINARY, etykieta);
   sprintf(etykieta,"R%02u.BI2H",addr);  present(CHILD_ID_BI2, S_BINARY, etykieta); 
   sprintf(etykieta,"R%02u.AI1",addr);   present(CHILD_ID_AI1, S_MOISTURE, etykieta);   
+  sprintf(etykieta,"R%02u.AO1",addr);   present(CHILD_ID_AO1, S_DIMMER, etykieta);
 }
 
 void receive(const MyMessage &message){
+  int percent = 0;
   switch (message.sensor) {
       case 1:
         if (message.getBool() == true){ 
@@ -78,6 +86,12 @@ void receive(const MyMessage &message){
         }else{
           BO_RESET();
         }
+      case 5:
+        percent = message.getInt(); 
+        moisture_set = percent;
+        send(msgAO1.set(moisture_set));
+        save_to_flash_after = millis() + 10000;
+        save_to_flash_flag = true;
       break;   
   }
 }
@@ -96,13 +110,24 @@ void setup(){
   int moisture_percent = map(analogRead(ai1),670,277,0,100);
   moisture = constrain(moisture_percent,0,100);
 
+  EEPROM.get(710, moisture_set);   
+  if(isnan(moisture_set) || moisture_set < 0){
+    moisture_set = 60;
+  }
+
   send(msgBO1.set(false,1));
   send(msgBI1.set(low_lvl,1));
   send(msgBI2.set(high_lvl,1));
   send(msgAI1.set(moisture, 0));
+  send(msgAO1.set(moisture_set, 0));
 }
 
 void loop(){
+  if(save_to_flash_flag == true && millis() > save_to_flash_after){
+    EEPROM.put(710,moisture_set);
+    save_to_flash_flag = false;
+  }
+
   if(bo_state[0] == true && (millis() - bo1_start_time) >= (impulse_time * impulse_ml)){
     BO_RESET();
   }
@@ -127,7 +152,7 @@ void loop(){
     }
   }
 
-    // przycisk
+  // przycisk
   if(digitalRead(bi3) == LOW){
     delay(10);
     if(digitalRead(bi3) == LOW){
@@ -144,7 +169,7 @@ void loop(){
     last_moisture_read = millis();   
   }
   
-  // raz na 5 minut wysyłka do domoticza
+  // raz na 5 minut wysyłka do domoticza i  wraazie potrzeby podlewanie
   if(bo_state[0] == false && (millis() - last_moisture_send) > 300000 ){
     send(msgAI1.set(moisture, 0));
     last_moisture_send = millis(); 
