b98b296fe15c9f51b96777123d3c2895ea528c28
Rysiek Labus <rysieklabus@gmail.com>
2021-03-29 17:16:49 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -177,6 +177,7 @@ void loop(){
     last_moisture_read = millis();   
   }
   
+  // test
   // raz na 5 minut wysyłka do domoticza i  wrazie potrzeby podlewanie automatyczne
   if(bo_state[0] == false && (millis() - last_moisture_send) > 300000 ){
     send(msgAI1.set(moisture, 0));
